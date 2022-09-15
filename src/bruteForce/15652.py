24d6aa741d122d838e5814175f35edd2440d52e9
suna-ji <jisuna314@gmail.com>
2022-09-15 22:14:59 +0900

    15652 n과m (4)

---
+++ src/bruteForce/15652.py
@@ -0,0 +1,21 @@
+# n과 m (4)
+# 같은수를 여러번 골라도 된다.
+# 오름차순
+
+# N과 M (2)
+# 1~N 까지 자연수 중 중복없이 M개를 고른 수열
+# 고른 수열은 오름차순이어야 한다. -> 순서가 중요
+
+import sys
+n, m = map(int, input().split())
+ans = [0] * m
+def select(index, start, n , m):
+    if index == m:
+        sys.stdout.write(' '.join(map(str, ans)) + '\n')
+        return
+    for i in range(start, n + 1):
+        ans[index] = i 
+        start = i
+        select(index + 1, start, n , m)
+
+select(0, 1, n, m)
