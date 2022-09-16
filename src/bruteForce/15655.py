1aa47a585473b6a90a7eda2d580a2c11e858fbaf
suna-ji <jisuna314@gmail.com>
2022-09-16 23:32:25 +0900

    15654 nm(6)

--- src/bruteForce/15655.py
+++ src/bruteForce/15655.py
@@ -1,3 +1,6 @@
+# n과 m(6)
+# 고른 수열은 오름차순이여야 한다.
+
 # N과 M (4)
 # N개의 서로 다른 자연수 중 M개를 고른 수열을 모두 구하자
 
@@ -11,16 +14,17 @@ check = [False] * 8
 ans = [0] * m
 list.sort() # 사전식으로 증가하는 순서로 출력
 # 로직
-def go(index, n, m):
+def go(index, start, n, m):
     if index == m:
         sys.stdout.write(' '.join(map(str, ans)) + '\n')
         return
-    for i in range(n):
+    for i in range(start, n):
         if check[i]:
             continue
         ans[index] = list[i]
         check[i] = True
-        go(index + 1, n, m)
+        go(index + 1, i + 1, n, m)
         check[i] = False
 
-go(0, n, m)
+go(0, 0, n, m)
+
